7572a6142ce4d8d5c7ba2627799261d45c48ad28
suissAI <jnascimento@gmail.com>
2026-09-23 19:27:55 -0300

    feat: export seccomp cgroup and zeroize primitives

--- src/lib.zig
+++ src/lib.zig
@@ -1,14 +1,18 @@
 pub const manifest = @import("manifest.zig");
 pub const worker = @import("worker.zig");
+pub const zeroize = @import("zeroize.zig");
 
 pub const linux = struct {
     pub const hardening = @import("linux/hardening.zig");
     pub const landlock = @import("linux/landlock.zig");
     pub const resources = @import("linux/resources.zig");
+    pub const seccomp = @import("linux/seccomp.zig");
+    pub const cgroup = @import("linux/cgroup.zig");
 };
 
 test {
     _ = manifest;
     _ = worker;
+    _ = zeroize;
     _ = linux;
 }
